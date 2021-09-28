b9138847a609957637b0ff9cec6657d98fc94b12
matheus cysneiros <msocys@gmail.com>
2021-09-28 15:31:44 -0300

    still in progress

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
-function trackUserHandler() {
-  console.log('Clicked!');
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(timerData, posData);
+  // .then((posData) => {
+  //   positionData = posData;
+  //   return setTimer(2000);
+  // })
+  // .catch((error) => console.log(error))
+  // .then((data) => console.log(data, positionData));
+
+  // setTimer(1000).then(() => console.log('Timer done'));
+
+  // console.log('Getting position....');
 }
 
 button.addEventListener('click', trackUserHandler);
 
-let result = 0;
-for (let i = 0; i < 100000000; i++) {
-  result += i;
-}
+// let result = 0;
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
 
-console.log(result);
+// console.log(result);
